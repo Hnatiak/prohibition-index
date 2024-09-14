6741f578be5661c0d2e4515621ef5c7182c998f1
Hnatiak <58102343+Hnatiak@users.noreply.github.com>
2024-09-15 01:13:45 +0200

    Prohibition Index v1.1

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowseRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowseRouter basename={"/prohibition-index"}>
+      <App />
+    </BrowseRouter>
   </React.StrictMode>
 );
